dd2130b413344335ebcc6d4b26da9e33586a4d02
pia fierro <piafierro.3187@gmail.com>
2022-12-05 14:23:36 +1000

    id's added to html elements and started javaScript file.

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+var viewScores = document.getElementById("view-scores");
+var timer = document.getElementById("timer");
+var startQuizButton = document.getElementById("start-quiz-button");
+var submitButton = document.getElementById("submit-button");
+var goBackButton = document.getElementById("go-back-btn");
+var clearHiscore = document.getElementById("clear-highscore");
+var timerCount;
+var timeLeft = 75;
+
