87be18f654be46e21bda4b86b4b62c827f2712be
pia fierro <piafierro.3187@gmail.com>
2022-12-09 22:08:24 +1000

    added: html list element,  basic css style and progressed JS.

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,23 @@
 //variables declaration in gloval scope.
+var topScreen = document.querySelector("#topScreen");
+var viewScores = document.querySelector("#view-scores");
+var timer = document.querySelector("#timer");
+var firstScreen = document.querySelector("#first-screen");
+var startQuizButton = document.querySelector("#start-quiz-button");
+var questionsScreen = document.querySelector("#questions-screen");
+var finishScreen = document.querySelector("#finish-screen");
+var finaleScore= document.querySelector("#finale-score");
+var submitButton = document.querySelector("#submit-button");
+var scoreScreen = document.querySelector("#score-screen");
+var highScores = document.querySelector("#high-scores");
+var goBackButton = document.querySelector("#go-back-btn");
+var clearHighScoreButton = document.querySelector("#clear-highscore-btn");
 
-var viewScores = document.getElementById("view-scores");
-var timer = document.getElementById("timer");
-var startQuizButton = document.getElementById("start-quiz-button");
-var finaleScore= document.getElementById("finale-score");
-var submitButton = document.getElementById("submit-button");
-var goBackButton = document.getElementById("go-back-btn");
-var clearHiscoreButton = document.getElementById("clear-highscore-btn");
 var timerCount;
 var timeLeft = 75;
-var questionNumber =0 ;
-var choiceLenght =4;
-var score = 0;
+var currentQuestionsIndex = 0;
 
-
-
+// questions array:
 var questions = [
 {
     title:"Commonly used data types do not include:",
@@ -43,45 +46,65 @@ var questions = [
 }
 ]
 
-//function declarations:
+//function declarations for the quizz timer and render questions:
 
-//timer
+//first screen to start quizz
+function startQuiz () {
+    firstScreen.classList.add("hide");
+    questionsScreen.classList.remove("hide");
+    setTimer();
+    renderQuestions();
+}
+
+//start the quiz when presing star button:
+startQuizButton.addEventListener("click",startQuiz); 
+
+//function that creates the timer
 function setTimer () {
-    timerCount = setInterval(function() {
+    var timerCount = setInterval(function() {
     timer.textContent =timeLeft + "s";
     timeLeft= timeLeft - 1;
     if (timeLeft===0) {
+        //clear timer and stop function
         clearInterval(timerCount);
         timer.textContent=""
-
+        endQuiz();
     }
     if (timeLeft===questions.length) {
         clearInterval(timerCount);
         timer.textContent=""
-
+        endQuiz();
     }    
     }, 1000);
 }
 
-//function to start the quiz when pressing button start
-function startQuiz () {
-    questionNumber=0;
-    score=0;
-    setTimer()
-    setquestions()
+//function to render questionns after pressing start button.
+function renderQuestions() {
+    scoreScreen.classList.add("hide");
+    
+    var currentQuestions = questions[currentQuestionsIndex].title;
+    var titleEl = document.getElementById("questions");
+    titleEl.textContent = currentQuestions;
+
+    for (i = 0; i < questions[currentQuestionsIndex].options.length; i++) {
+    var buttonEl = document.getElementById ("button" + (i + 1));
+    buttonEl.textContent = questions[currentQuestionsIndex].options[i];
+    buttonEl.addEventListener("click", answerCheck)
+    }
+}
+
+function answerCheck () {
 
 }
-//funtion for when the time gets to 0 or when all the answer are aswered before the timer reaching to 0
+
+//funtion for when the time gets to 0 or when all the answer are aswered before the timer reach to 0 s.
 function endQuiz() {
     localStorage.setItem("score",JSON.stringify(score));
     finaleScore.textContent = score
-
-
 }
 
 
-//add event listerner to the buttons
-startQuizButton.addEventListener("click",startQuiz); 
-submitButton 
-goBackButton 
-clearHiscoreButton
+
+
+
+
